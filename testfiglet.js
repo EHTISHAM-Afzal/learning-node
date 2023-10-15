49fb3f97207beaef1f1098b743cc13a7e618ad89
EHTISHAM-Afzal <khaj3172@gmail.com>
2023-10-15 23:18:17 +0500

    first commit

---
+++ testfiglet.js
@@ -0,0 +1,28 @@
+var figlet = require("figlet");
+
+figlet("Hello World!!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
+
+figlet.text(
+    "Boo!",
+    {
+      font: "Ghost",
+      horizontalLayout: "default",
+      verticalLayout: "default",
+      whitespaceBreak: true,
+    },
+    function (err, data) {
+      if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+      }
+      console.log(data);
+    }
+  );
